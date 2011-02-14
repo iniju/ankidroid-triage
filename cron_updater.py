1eb602548b15e9fcf7e4ca98e7133b6ded726ec0
Kostas Spyropoulos <inigo.aldana@gmail.com>
2011-02-14 03:47:14 -0500

    cronjobs process in batches now so we don't exceed time limit, batch size configurable

--- cron_updater.py
+++ cron_updater.py
@@ -19,34 +19,71 @@ import os, logging, re
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
 
 from receive_ankicrashes import Bug
 
+class CronjobSettings(db.Model):
+	name = db.StringProperty(required=True)
+	step = db.IntegerProperty(required=True)
+	offset = db.IntegerProperty(required=True)
+
 class ScanIssues(webapp.RequestHandler):
+	settings_name = 'issueScanner'
+	default_step = 10
 	def get(self):
+		cronSettings_query = CronjobSettings.all()
+		cronSettings_query.filter('name=', ScanIssues.settings_name)
+		cs = None
+		if cronSettings_query.count(1) == 0:
+			logging.info('initializing scanner for issues')
+			cs = CronjobSettings(name = ScanIssues.settings_name, step = ScanIssues.default_step, offset = 0)
+			cs.put()
+		else:
+			cs = cronSettings_query.fetch(1)[0]
+
 		bugs_query = Bug.all()
 		bugs_query.filter('linked =', False)
+		if cs.offset > bugs_query.count(1000000):
+			cs_offset = 0
 		bugs = []
-		bugs = bugs_query.fetch(1000)
+		bugs = bugs_query.fetch(cs.step, cs.offset)
 		for bg in bugs:
 			issues = bg.findIssue()
 			if issues:
 				bg.issueName = issues[0]['id']
 				logging.info("ScanIssues: Autolinking bug " + str(bg.key().id()) + " to issue " + str(bg.issueName))
 				bg.put()
+		cs.offset += cs.step
+		cs.put()
 
 class UpdateStatusesPriorities(webapp.RequestHandler):
+	settings_name = 'statusScanner'
+	default_step = 10
 	def get(self):
+		cronSettings_query = CronjobSettings.all()
+		cronSettings_query.filter('name=', UpdateStatusesPriorities.settings_name)
+		cs = None
+		if cronSettings_query.count(1) == 0:
+			logging.info('initializing scanner for status')
+			cs = CronjobSettings(name = UpdateStatusesPriorities.settings_name, step = UpdateStatusesPriorities.default_step, offset = 0)
+			cs.put()
+		else:
+			cs = cronSettings_query.fetch(1)[0]
+
 		bugs_query = Bug.all()
-		#bugs_query.filter('issueName !=', None)
 		bugs_query.filter('linked =', True)
+		if cs.offset > bugs_query.count(1000000):
+			cs_offset = 0
 		bugs = []
-		bugs = bugs_query.fetch(1000)
+		bugs = bugs_query.fetch(cs.step, cs.offset)
 		logging.debug("Cron job updater, found " + str(bugs_query.count(1000000)) + " bugs")
 		for bg in bugs:
 			if bg.updateStatusPriority():
 				logging.debug("Updated status and/or priority for bug: '" + str(bg.key().id()) + "'")
 				bg.put()
+		cs.offset += cs.step
+		cs.put()
 		
 application = webapp.WSGIApplication(
 		[(r'^/cron_updater/status_priority?.*', UpdateStatusesPriorities),
